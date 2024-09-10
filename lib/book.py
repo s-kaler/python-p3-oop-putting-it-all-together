3d14036a20084cf5463542e2110cd89b8565fa63
Sundeep <sundeep.kaler@gmail.com>
2024-09-09 23:36:59 -0400

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title = "Title", page_count = 0):
+        self.title = title
+        if (isinstance(page_count, int)):
+            self.page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def get_title(self):
+        return self._title
+
+    def set_title(self, title):
+        self._title = title
     
-        
+    title = property(get_title, set_title)
+    
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if (isinstance(page_count, int)):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand = "Adidas", size = 1):
+        self.brand = brand
+        if isinstance(size, int):
+            self.size = size
+        else:
+            print("size must be an integer")
+    
+    def get_brand(self):
+        return self._brand
+    
+    def set_brand(self, brand):
+        self._brand = brand
+
+    brand = property(get_brand, set_brand)
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+    condition = 'Old'
+
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")
